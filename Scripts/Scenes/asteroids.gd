878e101686cf085f5554b64f1ad670ed79f7f5e2
Bibanaev.A.E <bibanaev.a.e@sblogistica.ru>
2023-11-27 11:05:19 +0500

    Smooth player moving and first asteroids

--- Scripts/Scenes/asteroids.gd
+++ Scripts/Scenes/asteroids.gd
@@ -2,17 +2,23 @@ extends Node2D
 
 
 const ASTEROID_ENTER_FROM_IN_SECONDS: float = 0.5
-const ASTEROID_ENTER_TO_IN_SECONDS: float = 2.0
+const ASTEROID_ENTER_TO_IN_SECONDS: float = 1.5
+
 const ASTEROID_SPEED_FROM: float = 200.0
 const ASTEROID_SPEED_TO: float = 400.0
+
 const ASTEROID_ROTATION_RADIANT_FROM: float = -7.0
 const ASTEROID_ROTATION_RADIANT_TO: float = 7.0
+
 const ASTEROID_QUIT_ADDITIONAL: float = 50.0
+
+const FIRST_ASTEROIDS_COUNT_FROM: int = 3
+const FIRST_ASTEROIDS_COUNT_TO: int = 5
 
 
 @onready var asteroids_root: Node2D = $Asteroids
-@onready var asteroids_points_root: Node2D = $Points
 @onready var asteroids_timer: Timer = $Timer
+@onready var full_width: float = float(ProjectSettings.get('display/window/size/viewport_width'))
 @onready var full_height: float = float(ProjectSettings.get('display/window/size/viewport_height'))
 
 
@@ -23,6 +29,7 @@ var asteroids_generator: RandomNumberGenerator = RandomNumberGenerator.new()
 
 
 func _ready():
+	__place_several_asteroids();
 	__start_asteroids_background_timer()
 
 
@@ -37,24 +44,38 @@ func _process(_delta):
 			continue
 
 
+func __place_several_asteroids() -> void:
+	var asteroid_position: Vector2
+	for i in asteroids_generator.randi_range(FIRST_ASTEROIDS_COUNT_FROM, FIRST_ASTEROIDS_COUNT_TO):
+		asteroid_position = Vector2(
+			asteroids_generator.randf_range(0, full_width),
+			asteroids_generator.randf_range(0, full_height)
+		)
+
+		__place_asteroid(asteroid_position)
+
+
 func __start_asteroids_background_timer() -> void:
 	asteroids_timer.wait_time = asteroids_generator.randf_range(ASTEROID_ENTER_FROM_IN_SECONDS, ASTEROID_ENTER_TO_IN_SECONDS)
 	asteroids_timer.start()
 
 
 func _on_timer_timeout():
-	var all_markers: Array = asteroids_points_root.get_children()
-	var asteroid_marker: Marker2D = all_markers[randi() % all_markers.size()]
+	var asteroid_position: Vector2 = Vector2(asteroids_generator.randf_range(0, full_width), -30.0)
 
+	__place_asteroid(asteroid_position)
+
+	__start_asteroids_background_timer()
+
+
+func __place_asteroid(asteroid_position: Vector2) -> void:
 	var asteroid_texture: Texture = asteroid_textures[randi() % asteroid_textures.size()]
 
 	var asteroid: AsteroidSprite2D = AsteroidSprite2D.new()
 	asteroid.texture = asteroid_texture
-	asteroid.position = asteroid_marker.position
+	asteroid.position = asteroid_position
 	asteroid.speed = asteroids_generator.randf_range(ASTEROID_SPEED_FROM, ASTEROID_SPEED_TO)
 	asteroid.rotation_radiant = asteroids_generator.randf_range(ASTEROID_ROTATION_RADIANT_FROM, ASTEROID_ROTATION_RADIANT_TO)
 	asteroid.modulate.a = 0.3
 
 	asteroids_root.add_child(asteroid)
-
-	__start_asteroids_background_timer()
--- Scripts/Scenes/player.gd
+++ Scripts/Scenes/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 3.0
 const ADDITIONAL_BORDER = 10.0
 
 
@@ -22,7 +22,10 @@ func _ready():
 func _physics_process(_delta):
 	var mouse_position: Vector2 = get_global_mouse_position()
 
-	position.x = min(max(left_border, mouse_position.x), right_border)
+	velocity = Vector2(mouse_position.x - global_position.x, 0) * SPEED
+
+	if (mouse_position.x > right_border and position.x >= right_border) or (mouse_position.x < left_border and position.x <= left_border):
+		velocity = Vector2.ZERO
 
 	move_and_slide()
 
