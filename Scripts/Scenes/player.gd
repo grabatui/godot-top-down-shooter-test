ac0aaf4eb56ff0d11ca57d65b11af589bcd5726c
Bibanaev.A.E <bibanaev.a.e@sblogistica.ru>
2023-11-30 11:07:45 +0500

    Shoot interval

--- Scripts/Scenes/player.gd
+++ Scripts/Scenes/player.gd
@@ -4,21 +4,26 @@ class_name Player
 
 const INITIAL_SPEED = 150.0
 const INITIAL_LASER_SPEED = 400.0
+const INITIAL_SHOOT_SECONDS_INTERVAL = 1.3
 const ADDITIONAL_BORDER = 10.0
 
 
 @onready var player_sprite: Sprite2D = $PlayerShip3Blue
 @onready var shoot_marker: Marker2D = $ShootMarker
+@onready var shoot_timer: Timer = $ShootTimer
 
 
 @export var speed: float = INITIAL_SPEED
 @export var laser_speed: float = INITIAL_LASER_SPEED
+@export var shoot_seconds_interval: float = INITIAL_SHOOT_SECONDS_INTERVAL
 @export var laser_projectile: Texture
 
 
 var player_size: Vector2
 var left_border: float
 var right_border: float
+
+var shoot_wait: bool = false
 
 
 func _ready():
@@ -28,8 +33,10 @@ func _ready():
 
 
 func _process(_delta):
-	if Input.is_action_just_pressed('shoot'):
+	if Input.is_action_pressed('shoot') and not shoot_wait:
 		SignalsBus.player_shoot.emit(shoot_marker.global_position)
+
+		__start_shoot_timer()
 
 
 func _physics_process(delta):
@@ -43,3 +50,13 @@ func _physics_process(delta):
 
 	move_and_slide()
 
+
+func __start_shoot_timer() -> void:
+	shoot_timer.wait_time = shoot_seconds_interval
+	shoot_timer.start()
+
+	shoot_wait = true
+
+
+func _on_shoot_timer_timeout():
+	shoot_wait = false
